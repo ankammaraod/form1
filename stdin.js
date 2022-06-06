613a64be356a23644f5a2ae61c30837753ebfef7
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-06-06 22:24:53 +0530

    implemented old idea working for phone number and address

--- stdin.js
+++ stdin.js
@@ -1,33 +1,71 @@
-const { Data } = require('./data.js');
-const { validate } = require('./validation.js');
-const questions = [
-  'enter you name : ',
-  'enter you dob : ',
-  'enter you hobbies : '
-]
+const display = (contents) => {
+  console.log(contents);
+}
 
 const main = () => {
+  const formData = {};
+  const junk = [
+    {
+      property: 'name',
+      description: 'Please enter your name : ',
+      parse: (name) => name.trim(),
+      validate: (name) => name.length > 5
+    },
+    {
+      property: 'dob',
+      description: 'please enter your dob : ',
+      parse: (dob) => dob.trim(),
+      validate: (dob) => /\d\d\d\d-\d\d-\d\d/.test(dob)
+    },
+    {
+      property: 'hobbies',
+      description: 'please enter your hobbies : ',
+      parse: (hobbies) => hobbies.trim().split(','),
+      validate: (hobbies) => hobbies.length > 1
+    },
+    {
+      property: 'mobileNumber',
+      description: 'please enter your mobile number : ',
+      parse: (number) => +number,
+      validate: (number) => ('' + number).length === 10
+    },
+    {
+      property: 'address1',
+      description: 'please enter address 1 : ',
+      parse: (add) => add.trim(),
+      validate: () => true
+    },
+    {
+      property: 'address1',
+      description: 'please enter your address 2 : ',
+      parse: (add) => add.trim(),
+      validate: () => true
+    }
+  ];
+
   process.stdin.setEncoding('utf8');
-  const keys = ['name', 'dob', 'hobbies'];
-  const data = new Data(keys);
 
   let index = 0;
-
-  console.log(questions[index]);
-
+  console.log(junk[index].description);
   process.stdin.on('data', (chunk) => {
+    const parsedContent = junk[index].parse(chunk);
 
-    if (validate(keys[index], chunk)) {
-      data.addInput(chunk.trim());
+    if (junk[index].validate(parsedContent)) {
+      const key = junk[index].property;
+      if (formData[key]) {
+        formData[key] += ', ' + parsedContent;
+      } else {
+        formData[key] = parsedContent;
+      }
       index++;
     }
 
-    if (index >= questions.length) {
+    if (index >= junk.length) {
       console.log('thank you')
-      data.toString();
+      display(formData);
       process.exit(0);
     }
-    console.log(questions[index]);
+    console.log(junk[index].description);
   })
 }
 
