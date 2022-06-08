e50b7fecbc266536d5903d3da654ccd8f12d361e
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-06-08 15:40:20 +0530

    Added tests for form

---
+++ test/testForm.js
@@ -0,0 +1,35 @@
+const { Form } = require('../src/form.js');
+const { Field } = require('../src/field.js');
+const assert = require('assert');
+
+describe('Form', () => {
+  const identity = (x) => x;
+  const alwaysTrue = () => true;
+  const nameField = new Field('name', 'enter your name', identity, alwaysTrue);
+  const dobField = new Field('dob', 'enter your dob', identity, alwaysTrue);
+
+
+  it('should give current prompt', () => {
+    const form = new Form([nameField]);
+    assert.deepStrictEqual(form.currPrompt(), 'enter your name : ');
+  });
+
+  it('should give next prompt', () => {
+    const form = new Form([nameField, dobField]);
+    form.nextPrompt()
+    assert.deepStrictEqual(form.currPrompt(), 'enter your dob : ');
+  });
+
+  it('should return true if quries are over', () => {
+    const form = new Form([nameField]);
+    form.nextPrompt();
+    assert.deepStrictEqual(form.areQueriesOver(), true);
+  });
+
+  it('should give entries of the prompts', () => {
+    const form = new Form([nameField]);
+    form.fillCurrentField('ankammarao');
+    assert.deepStrictEqual(form.getEntries(), { 'name': 'ankammarao' });
+  });
+
+});
